dce9c98261a532ff9850504daa019f5692081707
ChetanKumar-Sura <81322260+chetankumar-sura@users.noreply.github.com>
2022-03-19 23:05:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,21 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
-
+pcross_road =input('''You're at a cross road. where do you want to go? Type "left" or "right" ''')
+if cross_road == "left":
+  lake = input('''You've come to a lake.There is island in the middle of the lake.Type "wait" to wait for a boat.Type "swim" to swim across ''' )
+  if lake == "wait":
+    door = input("You arrive at the island unharmed.There is a house with 3 doors.One red,one yellow and one blue.Which colour do you choose? ")
+    if door == "yellow":
+      print("You found the treasure! You win!")
+    elif door == "red":
+      print("It's full of fire.Game Over")
+    elif door == "blue":
+      print("you enter a room of beasts.Game Over")
+    else:
+      print("Game Over")
+  else:
+    print("you get attacked by an angry trout.Game Over")
+else:
+  print("full into a hole Game Over")
+    
